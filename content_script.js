f8bcfe375699a92335ec9ddf0e9b902a1aa9c94d
matus-krizan <matus@krizan.se>
2022-03-29 11:06:24 +0200

    Add files via upload

--- content_script.js
+++ content_script.js
@@ -1,7 +1,9 @@
-const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a')
+const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a, figure')
 
 for (let i=0; i < text.length; i++) {
 	if (text[i].innerHTML.includes('Kyjiv')) {
-		text[i].innerHTML = text[i].innerHTML.replace('Kyjiv', 'Kyjev')
+		while (text[i].innerHTML.includes('Kyjiv')) {
+			text[i].innerHTML = text[i].innerHTML.replace('Kyjiv', 'Kyjev')
+		}
 	}
 }
